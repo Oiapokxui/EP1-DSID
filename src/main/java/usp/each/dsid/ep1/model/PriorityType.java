9efc5ad09b48350a9f995acab180a62170e96726
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-06-18 14:29:01 -0300

    Creating priority type enum

---
+++ src/main/java/usp/each/dsid/ep1/model/PriorityType.java
@@ -0,0 +1,25 @@
+package usp.each.dsid.ep1.model;
+
+public enum PriorityType {
+    FREE,
+    BEST_EFFORT_BATCH,
+    MID_TIER,
+    PRODUCTION_TIER,
+    MONITORING_TIER;
+
+    public static PriorityType fromInteger(final int priority) {
+        if(priority <= 99) {
+            return FREE;
+        }
+        else if(priority <= 115) {
+            return BEST_EFFORT_BATCH;
+        }
+        else if(priority <= 119) {
+            return MID_TIER;
+        }
+        else if(priority <= 359) {
+            return PRODUCTION_TIER;
+        }
+        return MONITORING_TIER;
+    }
+}

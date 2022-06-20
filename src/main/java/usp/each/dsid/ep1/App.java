f90f5bb1ac578256cf22d9510b49c04a2976dd6b
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-06-20 11:47:04 -0300

    Updating application class to reflect deleted code

--- src/main/java/usp/each/dsid/ep1/App.java
+++ src/main/java/usp/each/dsid/ep1/App.java
@@ -2,41 +2,30 @@ package usp.each.dsid.ep1;
 
 import static usp.each.dsid.ep1.utils.Constants.INSTANCES_FILE_PATH;
 
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import scala.math.BigDecimal;
-import usp.each.dsid.ep1.function.FilterOnlyPresentOptional;
-import usp.each.dsid.ep1.function.GetObject;
-import usp.each.dsid.ep1.function.MapCsvToCollection;
-import usp.each.dsid.ep1.function.MapCsvToInstance;
-import usp.each.dsid.ep1.model.Collection;
-import usp.each.dsid.ep1.model.EventFactory;
-import usp.each.dsid.ep1.model.Instance;
+import usp.each.dsid.ep1.model.SchemaFactory;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
-    @Autowired MapCsvToCollection mapCsvToCollection;
-
-    @Autowired MapCsvToInstance mapCsvToInstance;
-
-    @Autowired GetObject<Instance> getInstance;
-
-    @Autowired FilterOnlyPresentOptional<Instance> filterOnlyPresentInstances;
-
-    @Autowired GetObject<Collection> getCollection;
-
-    @Autowired FilterOnlyPresentOptional<Collection> filterOnlyPresentCollections;
+    private static final Logger log = LoggerFactory.getLogger(App.class);
 
     @Autowired JavaSparkContext sparkContext;
 
-    @Autowired EventFactory eventFactory;
+    @Autowired SparkSession sparkSession;
+
+    @Autowired SchemaFactory schemaFactory;
 
     public static void main(final String[] args) {
         final SpringApplication application = new SpringApplication(App.class);
@@ -45,11 +34,12 @@ public class App implements CommandLineRunner {
     }
 
     @Override public void run(final String[] args) {
-        final JavaRDD<Instance> instances = sparkContext.textFile(INSTANCES_FILE_PATH)
-                .map(mapCsvToInstance)
-                .filter(filterOnlyPresentInstances)
-                .map(getInstance);
-        final JavaRDD<BigDecimal> mem = instances.map(Instance::getMemoryResourcesRequested);
-        final String b = mem.reduce(BigDecimal::$plus).toString();
+        final Dataset<Row> instances = sparkSession.read().schema(schemaFactory.eventSchema()).csv(INSTANCES_FILE_PATH);
+        instances.createOrReplaceTempView("instance");
+        final long start = System.currentTimeMillis();
+        log.info("******************** Starting reduction");
+        final Double sum = sparkSession.sql("select sum(resource_requests_memory) from instance").first().getDouble(0);
+        final long elapsedTime = System.currentTimeMillis() - start;
+        log.info("****************** took {} milliseconds to find sum {} of column", elapsedTime, sum);
     }
 }

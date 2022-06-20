7d777fd35a2b31ae28bab91548fc654689f13c4d
oiapokxui <enrique.emanuel@usp.br>
2022-06-20 16:06:51 -0300

    Changing instances file path string

--- src/main/java/usp/each/dsid/ep1/utils/Constants.java
+++ src/main/java/usp/each/dsid/ep1/utils/Constants.java
@@ -2,7 +2,7 @@ package usp.each.dsid.ep1.utils;
 
 public class Constants {
     public final static String COLLECTIONS_FILE_PATH = "sample-traces/collection_events/*";
-    public final static String INSTANCES_FILE_PATH = "google-traces/instance_events/*";
+    public final static String INSTANCES_FILE_PATH = "sample-traces/instance_events/*";
     public final static String MASTER_URL = "local[*]";
     public final static String APP_NAME = "EP1-DSID";
     public final static String SCHEDULER_MODE = "spark.scheduler.mode";

e94f211232ac826066de1f865fd3572d4824046f
Lucas Vilela Aleixo <lk-vila@protonmail.com>
2022-07-02 00:43:48 -0300

    feat: reimplements FilterGroupsWithBothScheduleAndSubmit

--- src/main/java/usp/each/dsid/ep1/function/FilterGroupsWithBothScheduleAndSubmit.java
+++ src/main/java/usp/each/dsid/ep1/function/FilterGroupsWithBothScheduleAndSubmit.java
@@ -3,17 +3,29 @@ package usp.each.dsid.ep1.function;
 import org.apache.spark.api.java.function.Function;
 
 import scala.Tuple2;
+
+import java.util.HashSet;
+import java.util.Set;
 import usp.each.dsid.ep1.function.MapInstanceToSubset.InstanceSubset;
 import usp.each.dsid.ep1.model.EventType;
 
 public class FilterGroupsWithBothScheduleAndSubmit implements Function<Tuple2<Long, Iterable<InstanceSubset>>, Boolean> {
 
-    @Override public Boolean call(final Tuple2<Long, Iterable<InstanceSubset>> groups) throws Exception {
-        final Iterable<InstanceSubset> javaGroupIterable = groups._2;
-        final scala.collection.Iterable<InstanceSubset> objetos = scala.jdk.CollectionConverters.IterableHasAsScala(javaGroupIterable).asScala();
-        final Boolean anySubmit = objetos.find(obj -> obj.type().equals(EventType.SUBMIT)).isDefined();
-        final Boolean anySchedule = objetos.find(obj -> obj.type().equals(EventType.SCHEDULE)).isDefined();
-        return anySubmit && anySchedule;
+    @Override public Boolean call(final Tuple2<Long, Iterable<InstanceSubset>> group) throws Exception {
+        final Iterable<InstanceSubset> javaGroupIterable = group._2;
+        final Set<Long> submitIndexes = new HashSet<Long>();
+
+        for (InstanceSubset taskStep : javaGroupIterable) {
+            if (taskStep.type().equals(EventType.SUBMIT)) {
+                submitIndexes.add(taskStep.instanceIndex());
+            }
+        }
+
+        for (InstanceSubset taskStep : javaGroupIterable) {
+            if (taskStep.type().equals(EventType.SCHEDULE) && submitIndexes.contains(taskStep.instanceIndex())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
-

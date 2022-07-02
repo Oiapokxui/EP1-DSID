a05aa8660683c79cacbcb2a6729dab1e08e59790
oiapokxui <enrique.emanuel@usp.br>
2022-07-02 16:27:25 -0300

    Adding constants for samples files

--- src/main/java/usp/each/dsid/ep1/utils/Constants.java
+++ src/main/java/usp/each/dsid/ep1/utils/Constants.java
@@ -1,11 +1,10 @@
 package usp.each.dsid.ep1.utils;
 
 public class Constants {
-    //    public final static String COLLECTIONS_FILE_PATH = "sample-traces/collection_events/*";
-    //    public final static String INSTANCES_FILE_PATH = "google-traces/instance_events/*";
-
-    public final static String COLLECTIONS_FILE_PATH = "gs://borg-traces/sample-traces/collection_events/*";
-    public final static String INSTANCES_FILE_PATH = "gs://borg-traces/sample-traces/instance_events/*";
+    public final static String COLLECTIONS_SAMPLES_FILE_PATH = "gs://sample-traces/google-traces/collection_events/*";
+    public final static String INSTANCES_SAMPLES_FILE_PATH = "gs://sample-traces/google-traces/instance_events/*";
+    public final static String COLLECTIONS_FILE_PATH = "gs://borg-traces/google-traces/collection_events/*";
+    public final static String INSTANCES_FILE_PATH = "gs://borg-traces/google-traces/instance_events/*";
     public final static String INSTANCE_HEADER = "time,type,collection_id,priority,instance_index,resource_request.cpus,resource_request.memory";
     public final static String COLLECTION_HEADER = "time,type,collection_id,priority";
     public final static String LOCAL_STANDALONE_MASTER_URL = "local[*]";
